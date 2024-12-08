67f0fb74600e320a430878f6a5bd7907b683c761
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-12-08 14:25:01 +0000

    Added commented out code for educational purposes

--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -26,3 +26,31 @@ export default class StatefulGreeting extends React.Component {
     );
   }
 }
+
+/* component has a construvtor which sets the state of the class
+
+import React from "react";
+
+class StatefulGreeting extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting;
+*/
